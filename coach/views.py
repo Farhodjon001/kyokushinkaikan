17f09809e7091066d84070d8697deee14f2a3531
uzbek <uzbekbest0@gmail.com>
2023-09-10 16:06:35 +0500

    committed

--- coach/views.py
+++ coach/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .serializers import CouchSerializers
+from rest_framework import generics
+from .models import CoachModel
 # Create your views here.
+class DetailCoachView(generics.RetrieveAPIView):
+    queryset = CoachModel.objects.all()
+    serializer_class = CouchSerializers
+
+# class DetailCoachView(APIView):
+#     def get(self,request,*args,**kwargs):
+#         Coach = get_object_or_404(CoachModel, id=kwargs['id'])
+#         serializer = TrainersSerializers(Coach)
+#         return Response(serializer.data)
+class CreateCoachView(generics.CreateAPIView):
+    queryset = CoachModel.objects.all()
+    serializer_class = CouchSerializers
+class ListCoachView(generics.ListAPIView):
+    queryset = CoachModel.objects.all()
+    serializer_class = CouchSerializers
+
+class DeleteCoachView(generics.DestroyAPIView):
+    queryset = CoachModel.objects.all()
+    serializer_class = CouchSerializers
